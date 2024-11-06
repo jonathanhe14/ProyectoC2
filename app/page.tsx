4bdd1fbd0dfa27e261b81ed700ed7d129a5f623b
Jonathan <jonaherrera90@hotmail.com>
2024-11-06 17:17:42 -0600

    2Solucionando problema de despliegue

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,7 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
-import { AppProps } from "next/app";
 import { useRouter } from "next/navigation";
-
 
 Amplify.configure(outputs);
 
@@ -18,19 +16,17 @@ const client = generateClient<Schema>();
 
 const adminEmails = ["jonaherrera90@hotmail.com"];
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function HomePage() {
   const { user, route } = useAuthenticator((context) => [context.user, context.route]);
   const router = useRouter();
 
   // Si no está autenticado, redirige al usuario a la página de inicio de sesión
   useEffect(() => {
-    console.log(route);
     if (route !== 'authenticated') {
-      console.log(user?.signInDetails?.loginId);
       router.push('/sign-in');
-    }else(
-      router.push('/home')
-    )
+    } else {
+      router.push('/home');
+    }
   }, [route, router]);
 
   if (route !== 'authenticated') {
@@ -43,3 +39,4 @@ export default function App({ Component, pageProps }: AppProps) {
     </div>
   );
 }
+
